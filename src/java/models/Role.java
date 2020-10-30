0c8a4a221cac8199bbfdf299bd824005e5f43c5b
775653 <775653@SAIT224373IT.ACDM.DS.SAIT.CA>
2020-10-30 10:06:49 -0600

    Updated Role.java coded this file

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -5,10 +5,42 @@
  */
 package models;
 
+import java.io.Serializable;
+
 /**
  *
  * @author 775653
+ * 
  */
-public class Role {
+public class Role implements Serializable  {
+    private int roleNum;
+    private String roleDesc;
+
+    public Role() {
+        
+    }
+
+    public Role(int roleNum, String roleDesc) {
+        this.roleNum = roleNum;
+        this.roleDesc = roleDesc;
+    }
+
+    public int getRoleNum() {
+        return roleNum;
+    }
+
+    public void setRoleNum(int roleNum) {
+        this.roleNum = roleNum;
+    }
+
+    public String getRoleDesc() {
+        return roleDesc;
+    }
+
+    public void setRoleDesc(String roleDesc) {
+        this.roleDesc = roleDesc;
+    }
+    
+    
     
 }
