94674222cee63d383f36c08689e3dcc30a38eabc
813033 <813033@SAIT225758IT.ACDM.DS.SAIT.CA>
2020-10-30 10:25:38 -0600

    Fixed errors.

--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -15,28 +15,28 @@ import models.User;
  */
 public class UserService {
     
-    public List<User> getAll(String email) throws Exception {
+    public List<User> getAll() throws Exception {
         UserDB userDB = new UserDB();
-        List<User> users = userDB.getAll(email);
+        List<User> users = userDB.getAll();
         return users;
     }
       
     public void insert(String email, int active, String firstName, String lastName, String password, int role) throws Exception {
         User user = new User(email, active, firstName, lastName, password, role);
-        UserDB noteDB = new UserDB();
+        UserDB userDB = new UserDB();
         userDB.insert(user);
     }
     
     public void update(String email, int active, String firstName, String lastName, String password, int role) throws Exception {
-        User note = new User(email, active, firstName, lastName, password, role);
-        UserDB noteDB = new UserDB();
+        User user = new User(email, active, firstName, lastName, password, role);
+        UserDB userDB = new UserDB();
         userDB.update(user);
     }
     
     public void delete(String email) throws Exception {
         User user = new User();
         user.setEmail(email);
-        UserDB noteDB = new UserDB();
+        UserDB userDB = new UserDB();
         userDB.delete(user);
     }
     
