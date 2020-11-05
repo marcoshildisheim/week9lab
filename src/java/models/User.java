1d10dd07b4dc0a5521513f58fb4d5020d6f0371d
820203 <820203@SAIT224250IT.ACDM.DS.SAIT.CA>
2020-11-04 23:26:11 -0700

    Modifications to src/models/User.java

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -1,85 +1,162 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 import java.io.Serializable;
+import java.util.List;
+import javax.persistence.*;
+import javax.xml.bind.annotation.*;
 
 /**
  *
  * @author 775653
  */
-public class User implements Serializable {
+@Entity
+@Table(name = "user")
+@XmlRootElement
+@NamedQueries
+    ({
+      @NamedQuery(name = "User.findAll", query = "SELECT u FROM User u"),
+      @NamedQuery(name = "User.findByEmail", query = "SELECT u FROM User u WHERE u.email = :email"),
+      @NamedQuery(name = "User.findByActive", query = "SELECT u FROM User u WHERE u.active = :active"),
+      @NamedQuery(name = "User.findByFirstName", query = "SELECT u FROM User u WHERE u.firstName = :firstName"),
+      @NamedQuery(name = "User.findByLastName", query = "SELECT u FROM User u WHERE u.lastName = :lastName"),
+      @NamedQuery(name = "User.findByPassword", query = "SELECT u FROM User u WHERE u.password = :password")            
+    })
+
+public class User implements Serializable 
+{
+    private static final long serialVersionUID = 1L;
     
+    @Id
+    @Basic(optional = false)
+    @Column(name = "email")
     private String email;
-    private int active;
-    private String firstname;
-    private String lastname;
-    private String password;
-    private int role;
     
-    public User(){
+    @Basic(optional = false)
+    @Column(name = "active")
+    private boolean active;
+    
+    @Basic(optional = false)
+    @Column(name = "first_name")
+    private String firstName;
+    
+    @Basic(optional = false)
+    @Column(name = "last_name")
+    private String lastName;
+    
+    @Basic(optional = false)
+    @Column(name = "password")
+    private String password;
+    
+    @JoinColumn(name = "role", referencedColumnName = "role_id")
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private Role role;
+    
+    public User()
+    {
         
     }
     
-    public User(String email, int active, String firstName, String lastName, String password, int role) {
+    public User(String email)
+    {
+        this.email = email;
+    }
+    
+    public User(String email, boolean active, String firstName, String lastName, String password)
+    {
         this.email = email;
         this.active = active;
-        this.firstname = firstName;
-        this.lastname = lastName;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.password = password;
-        this.role = role;
     }
 
-    public String getEmail() {
+    public String getEmail() 
+    {
         return email;
     }
 
-    public void setEmail(String email) {
+    public void setEmail(String email) 
+    {
         this.email = email;
     }
 
-    public int getActive() {
+    public boolean isActive() 
+    {
         return active;
     }
 
-    public void setActive(int active) {
+    public void setActive(boolean active) 
+    {
         this.active = active;
     }
 
-    public String getFirstName() {
-        return firstname;
+    public String getFirstName() 
+    {
+        return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstname = firstName;
+    public void setFirstName(String firstName) 
+    {
+        this.firstName = firstName;
     }
 
-    public String getLastName() {
-        return lastname;
+    public String getLastName() 
+    {
+        return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.firstname = lastName;
+    public void setLastName(String lastName) 
+    {
+        this.lastName = lastName;
     }
 
-    public String getPassword() {
+    public String getPassword() 
+    {
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password) 
+    {
         this.password = password;
     }
 
-    public int getRole() {
+    public Role getRole() 
+    {
         return role;
     }
 
-    public void setRole(int role) {
+    public void setRole(Role role) 
+    {
         this.role = role;
     }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 0;
+        hash += (email != null ? email.hashCode() : 0);
+        return hash;        
+    }
     
+    @Override
+    public boolean equals(Object object)
+    {
+        if(!(object instanceof Role))
+        {
+            return false;
+        }
+        Role other = (Role) object;
+        
+        if((this.email == null && other.email != null) || (this.email != null && !this.email.equals(other.email)))
+        {
+            return false;
+        }
+        return true;
+    }
     
+    @Override 
+    public String toString()
+    {
+        return "models.User[ email=" + email + " ]";
+    }
 }
